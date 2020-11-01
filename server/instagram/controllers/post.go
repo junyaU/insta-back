db5195d7d77ed79160bab14ce620ae5002dc769d
junyaUch <junya19991215@icloud.com>
2020-11-01 13:45:38 +0900

    ユーザー紐付けとセッションの途中まで

--- server/instagram/controllers/post.go
+++ server/instagram/controllers/post.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"instagram/models"
-	"log"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
@@ -15,7 +14,7 @@ type PostController struct {
 func (this *PostController) GetAllPosts() {
 	o := orm.NewOrm()
 	var allPosts []models.Post
-	o.QueryTable(new(models.Post)).All(&allPosts)
+	o.QueryTable(new(models.Post)).RelatedSel().All(&allPosts)
 
 	this.Data["json"] = allPosts
 
@@ -23,24 +22,29 @@ func (this *PostController) GetAllPosts() {
 }
 
 func (this *PostController) Post() {
+	session := this.StartSession()
+	userId := session.Get("UserId")
+	Name := session.Get("Name")
+	Email := session.Get("Email")
+
+	if userId == nil || Name == nil || Email == nil {
+		this.Redirect("/", 302)
+		return
+	}
+
+	id := userId.(int64)
+
 	inputComment := this.GetString("Comment")
 	inputImage := this.GetString("Image")
 
-	o := orm.NewOrm()
 	post := models.Post{
 		Comment: inputComment,
-		User:    &models.User{Id: 1},
+		User:    &models.User{Id: id},
 		Image:   inputImage,
 	}
 
-	id, err := o.Insert(&post)
-
-	if err == nil {
-		log.Println(id)
-	} else {
-		log.Println(err)
-	}
+	o := orm.NewOrm()
+	o.Insert(&post)
 
 	this.Redirect("/posthome", 302)
-
 }
--- server/instagram/main.go
+++ server/instagram/main.go
@@ -7,11 +7,29 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	"github.com/astaxie/beego/session"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	setupDB()
+
+	sessionconf := &session.ManagerConfig{
+		CookieName: "beegosessionId",
+		Gclifetime: 3600,
+	}
+	beego.GlobalSessions, _ = session.NewManager("memory", sessionconf)
+	go beego.GlobalSessions.GC()
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}))
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
